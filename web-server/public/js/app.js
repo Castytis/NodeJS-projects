c4681699ff29d55b39d901dc6f0e853b84b7d37d
Kastytis S <kastytis.slubelis@gmail.com>
2022-02-24 22:26:21 +0200

    fetch forecast

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,24 @@
-console.log("test")
+const weatherForm = document.querySelector('.location-form');
+const searchLocation = document.querySelector('.location-input');
+
+const locationMessage = document.querySelector('#location-p');
+const forecastMessage = document.querySelector('#forecast-p');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = searchLocation.value;
+    
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error){
+                locationMessage.textContent = data.error;
+                forecastMessage.textContent = '';
+            } else {
+                locationMessage.textContent = data.location;
+                forecastMessage.textContent = data.forecast;
+            }
+        })
+    })
+})
