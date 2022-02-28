4aa032333f2cb0ea4eed51972e8abed978d741f9
Kastytis S <kastytis.slubelis@gmail.com>
2022-02-28 17:09:49 +0200

    patch endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
 require('./db/mongoose')
+const express = require('express');
 const User = require('./models/user');
 const Task = require('./models/task')
 
@@ -19,17 +19,6 @@ app.post('/users', async (req, res) => {
     }
 })
 
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
 app.get('/users', async (req, res) => {
    
    try {
@@ -41,6 +30,7 @@ app.get('/users', async (req, res) => {
 })
 
 app.get('/users/:id', async (req, res) => {
+    
     const _id = req.params.id;
 
     try {
@@ -51,6 +41,42 @@ app.get('/users/:id', async (req, res) => {
         res.send(user)
     } catch (e) {
         res.status(500).send()
+    }
+})
+
+app.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if(!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try{
+        const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true})
+        
+        if(!user) {
+            return res.status(404).send()
+        }
+        
+        res.send(user)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
     }
 })
 
@@ -70,6 +96,30 @@ app.get('/tasks/:id', async(req, res) => {
     try {
         const task = await Task.findById(_id)
         if (!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update)=> {
+        return allowedUpdates.includes(update);
+    })
+
+    if (!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true})
+    
+        if (!task) {
             return res.status(404).send()
         }
         res.send(task)
