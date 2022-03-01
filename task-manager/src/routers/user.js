a31143b5f950d9b2b31dc5db76faf9e85c240425
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-01 10:40:49 +0200

    generate auth tokens

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -7,7 +7,9 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
     
     try {
-        await user.save()
+
+        const token = await user.generateAuthToken()
+        await user.save({user, token})
         res.status(201).send(user)
     } catch (e) {
         res.status(400).send(e)
@@ -89,7 +91,9 @@ router.delete('/users/:id', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+
+        const token = await user.generateAuthToken()
+        res.send({user, token})
     } catch (e) {
         res.status(400).send()
     }
