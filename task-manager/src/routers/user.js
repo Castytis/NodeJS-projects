cdb5f92f138a2b08af31c6f0fa8089f0cf7bb461
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-02 17:45:50 +0200

    file upload, fetch, delete

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,4 +1,20 @@
 const express = require('express');
+
+const sharp = require('sharp')
+const multer = require('multer')
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpeg|jpg|png)$/)){
+            return cb(new Error('Please upload jpeg, jpg or png'))
+        }
+        cb(undefined, true)
+    }
+
+})
+
 const User = require('../models/user');
 
 const auth = require('../middleware/auth')
@@ -88,5 +104,36 @@ router.post('/users/logoutall', auth, async (req, res) => {
     }
 })
 
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+    
+},  (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) { 
+            throw new Error()
+        }
+       
+        res.set('Content-type', 'image/png')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
 
 module.exports = router;
