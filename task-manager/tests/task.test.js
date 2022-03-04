d8e910d46950877263f4c30585aa0c0e5f2b29e9
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-04 12:50:42 +0200

    testing tasks

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const { userOneId, userOne, setupDataBase } = require('./fixtures/db')
+const { userOneId, userOne, setupDataBase, userTwo, taskOne } = require('./fixtures/db')
 
 beforeEach(setupDataBase)
 
@@ -17,4 +17,23 @@ test('Should create task for user', async () => {
     const task = await Task.findById(response.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toBe(false)
+})
+
+test('Should get all users tasks', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        expect(200)
+    expect(response.body.length).toBe(2)
+})
+
+test('User should not delete other users tasks', async () => {
+    await request(app)
+        .delete('/tasks/'+taskOne._id)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        expect(404)
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
 })
