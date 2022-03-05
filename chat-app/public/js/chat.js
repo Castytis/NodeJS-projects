58644cd78bb5aba3e1648b61f7af96594a043302
Kastytis S <kastytis.slubelis@gmail.com>
2022-03-05 20:54:45 +0200

    Location sharing, broadcasting

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,6 +1,6 @@
 const socket = io()
 
-socket.on('welcomeMessage', (message) => {
+socket.on('message', (message) => {
     console.log(message)
 })
 
@@ -8,7 +8,19 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
     
     const message = e.target.elements.message.value
-    console.log(message)
     
     socket.emit('sendMessage', message)
+})
+
+document.querySelector('#share-location').addEventListener('click', () =>{
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        let location = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }
+        socket.emit('sendLocation', location)
+    })
 })
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -13,15 +13,26 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-    console.log('New web socket connection')
+    console.log('New WebSocket connection')
 
-    socket.emit('welcomeMessage', 'Welcome')
+    socket.emit('message', 'Welcome!')
+
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
-       io.emit('message', message)
-        
+        io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+
+    socket.on('sendLocation', (location) => {
+        io.emit('message', `https://www.google.com/maps?q=${location.latitude},${location.longitude}`)
     })
 })
+
+
 
 server.listen(port, () => {
     console.log('Server started on port ' + port)
